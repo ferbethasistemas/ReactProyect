8a17a8dfc5cb290d5d1e6e9aeb02ee3fde342886
Fernando <bethancourt@alice.it>
2019-04-05 11:14:33 -0600

    Proyecto Crear-App-React utilizando npm

---
+++ public/precache-manifest.f4f95d5d7dd25917931fc3857b24f51f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "150eafa0aa7d9f0450fe",
+    "url": "/static/js/main.5c4c5e96.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "150eafa0aa7d9f0450fe",
+    "url": "/static/css/main.e1a60774.chunk.css"
+  },
+  {
+    "revision": "e122bd3716a60cb702303492244bb47e",
+    "url": "/index.html"
+  }
+];
